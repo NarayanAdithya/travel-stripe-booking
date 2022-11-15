89dd04b842f6cb4f4a22a7c207d571dea22f3776
NarayanAdithya <narayanadithya1234@gmail.com>
2022-11-15 23:49:39 +0530

    made the change

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ print(load_dotenv())
 
 class config(object):
     SECRET_KEY= os.environ.get('SECRET_KEY') or '334nadnj&&89Ydau89YAd98adbszmdi3*&&923kln'
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///'+os.getcwd()+'/app.db'
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI') or 'sqlite:///'+os.getcwd()+'/app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     GOOGLE_CLIENT_ID=os.environ.get('CLIENT_ID')
     GOOGLE_CLIENT_SECRET=os.environ.get('CLIENT_SECRET')
