81a3ef94baef82e176654aa5d1a03e35056e8a17
NarayanAdithya <narayanadithya1234@gmail.com>
2022-11-18 00:31:13 +0530

    Updated UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./app/templates/**/*.html",
-    "./app/static/src/**/*.js"
+    "./app/static/src/**/*.js",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
